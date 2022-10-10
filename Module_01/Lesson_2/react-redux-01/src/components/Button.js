540fc098ed464b3ea71bac3a43b220cece4fa5a8
rajibhasan01 <hasan.rajib1996@gmail.com>
2022-10-10 19:32:11 +0600

    updated state for multiple state management

---
+++ Module_01/Lesson_2/react-redux-01/src/components/Button.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Button = ({children, handler}) => {
+  return (
+    <button
+    class="bg-indigo-400 text-white px-3 py-2 rounded shadow"
+    id="increment" onClick={handler}
+  >
+    {children}
+  </button>
+  )
+}
+
+export default Button
--- Module_01/Lesson_2/react-redux-01/src/components/Counter.js
+++ Module_01/Lesson_2/react-redux-01/src/components/Counter.js
@@ -1,34 +1,17 @@
-import React, { useState } from "react";
+import React from "react";
+import Button from "./Button";
+import Count from "./Count";
 
-const Counter = () => {
-    const [count, setCount] = useState(0);
-
-    const increment = () => {
-        setCount((prevCount) => prevCount + 1);
-    };
-
-    const decrement = () => {
-        setCount((prevCount) => prevCount - 1);
-    };
+const Counter = ({id, count, increment, decrement}) => {
 
   return (
     <div class="p-4 h-auto flex flex-col items-center justify-center space-y-5 bg-white rounded shadow">
       <div class="text-2xl font-semibold" id="counter">
-        {count}
+        <Count count={count}/>
       </div>
       <div class="flex space-x-3">
-        <button
-          class="bg-indigo-400 text-white px-3 py-2 rounded shadow"
-          id="increment" onClick={increment}
-        >
-          Increment
-        </button>
-        <button
-          class="bg-red-400 text-white px-3 py-2 rounded shadow"
-          id="decrement" onClick={decrement}
-        >
-          Decrement
-        </button>
+        <Button handler={()=> increment(id)}>Increment</Button>
+        <Button handler={()=> decrement(id)}>Decrement</Button>
       </div>
     </div>
   );
